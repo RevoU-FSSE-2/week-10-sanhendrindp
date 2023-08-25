3403b29549457bd38ae1c3a752ec3cf46fb7e82a
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-08-25 23:16:31 +0700

    feat: :rocket: Deploy MongoDB with railway

--- middleware/database-middleware.js
+++ middleware/database-middleware.js
@@ -2,9 +2,11 @@ const { MongoClient } = require("mongodb");
 
 let db;
 const databaseMiddleware = async (req, res, next) => {
-  // Connect to mongodb
+  // Connect to MongoDB
+  // default local: mongodb://127.0.0.1:27017
+  // Atlas MongoDB: mongodb+srv://sanhendrindwip:MzDgt5ZYeL9MvvTv@cluster0.elyckcb.mongodb.net/?retryWrites=true&w=majority
   const mongoClient = await new MongoClient(
-    "mongodb://127.0.0.1:27017"
+    "mongodb://mongo:ca4Kdg7CTuCtMz0ERGNm@containers-us-west-85.railway.app:6938"
   ).connect();
   db = mongoClient.db("revou-week-10");
 
